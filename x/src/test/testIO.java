d503bb06c3c81e84691facfc09eaea48950bf99c
Mr.zero <fordayscounting@gmail.com>
2014-12-11 09:18:40 +0800

    12-11

---
+++ x/src/test/testIO.java
@@ -0,0 +1,16 @@
+package test;
+
+import java.io.File;
+import java.io.IOException;
+
+import IO.Praticse.inputStreamU;
+
+public class testIO {
+	public static void main(String[] args) throws IOException {
+	
+		inputStreamU a = new inputStreamU();
+		
+		a.inputFile(new File("//workSpace//xixi.txt"));
+	}
+    
+}
